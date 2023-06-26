c93484f053ffc73ec103a832ac825407387d8415
Bharat Bhammar <bhartbhammar3336@gmail.com>
2023-06-27 00:29:57 +0530

    t7

--- utils/fetchPageInfo.ts
+++ utils/fetchPageInfo.ts
@@ -1,13 +1,16 @@
 import { urlFor } from "@/sanity";
 import { PageInfo, Skill } from "@/typings";
+import axios from "axios";
 
 export const fetchPageInfo = async () => {
-    const res = await fetch(
-        `https://next-portfolio-o1lvkfe7z-bharat0509.vercel.app/api/getPageInfo`
-    );
-    if (!res.ok) return {};
-    const data = await res.json();
-    const pageInfo: PageInfo = data;
+    const res = await axios
+        .get(
+            `https://next-portfolio-o1lvkfe7z-bharat0509.vercel.app/api/getPageInfo`
+        )
+        .then((res) => res.data);
+
+    // const data = res;
+    const pageInfo: PageInfo = res;
     let imgUrl = urlFor(pageInfo.heroImage).url();
     pageInfo.img = imgUrl;
     return pageInfo;
