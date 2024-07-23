15ea29876825fe9888259b5c5d676e03a1b2068a
bharat <bhartbhammar3336@gmail.com>
2024-07-23 17:41:16 +0530

    Metadata updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,26 +8,38 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
     metadataBase: new URL("https://bharat001.me/"),
-    title: "Bharat Bhammar - SWE | UX",
+    title: "Bharat Bhammar - Aspiring Software Engineer | Full Stack Developer",
     description:
-        "Frontend & Mobile App Developer from Pakistan with 3+ years of expertise. Senior Software Engineer. Specializing mobile apps, UX, and JavaScript technologies.",
+        "Aspiring Software Engineer currently pursuing B.Tech with a passion for AI, machine learning, and full-stack development. Experienced in developing AI-powered applications, enhancing network management operations, and creating scalable web solutions. Proficient in React, Node.js, and natural language processing.",
     keywords: [
-        "Developer",
-        "Portfolio",
-        "Developer Portflio",
+        "Bhammar",
         "Bharat",
         "Bharat Bhammar",
-        "Bhammar",
-        "Next.js",
-        "React",
-        "ReactNative",
-        "Android",
+        "Aspiring Software Engineer",
+        "AI Enthusiast",
+        "Machine Learning",
+        "B.Tech Student",
+        "Full Stack Developer",
+        "AI-powered applications",
+        "React Developer",
+        "Node.js Developer",
+        "NLP",
+        "Natural Language Processing",
+        "Portfolio",
+        "JavaScript Developer",
+        "Python Developer",
+        "Software Intern",
+        "Cisco Intern",
+        "Frontend Developer",
+        "Backend Developer",
+        "Web Developer",
+        "Tech Enthusiast",
     ],
     openGraph: {
-        title: "Bharat Bhammar",
+        title: "Bharat Bhammar - Aspiring Software Engineer | Full Stack Developer",
         description:
-            "Frontend & Mobile App Developer from Pakistan with 3+ years of expertise. Senior Software Engineer. Specializing mobile apps, UX, and JavaScript technologies.",
-        images: "/OpenGraph.jpg",
+            "Aspiring Software Engineer currently pursuing B.Tech with a passion for AI, machine learning, and full-stack development. Experienced in developing AI-powered applications, enhancing network management operations, and creating scalable web solutions. Proficient in React, Node.js, and natural language processing. Notable projects include ShopSwift, ChatCat, and Image Art Generator.",
+        images: "/bharat-logo.png",
     },
     alternates: {
         canonical: "https://bharat001.me",
@@ -44,7 +56,7 @@ export default function RootLayout({
             <body
                 className={`${inter.className} bg-[var(--bgColor)] overflow-y-scroll overflow-x-hidden`}
             >
-                <NavBar/>
+                <NavBar />
                 <StarsCanvas />
                 {children}
             </body>
